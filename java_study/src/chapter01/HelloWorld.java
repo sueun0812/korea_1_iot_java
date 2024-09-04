418abb655d12162e8c005253404021c1a2dff749
sueun0812 <plok9408@naver.com>
2024-09-04 16:08:13 +0900

    20240904

---
+++ java_study/src/chapter01/HelloWorld.java
@@ -0,0 +1,26 @@
+package chapter01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//		자바의 파일 단위
+		//		- 클래스 파일을 생성하여 코드 작성
+		//			: 파일명 UpperCamelCase 사용
+		//		- 파일의 확장자 : .java
+		
+		
+		//		 기본 출력문 단축키
+		//		syso
+		//		sysout
+		
+		//		실행 단축키
+		//		- comment + f11
+		
+		System.out.println("Hello World");
+	 	
+		
+		
+
+	}
+
+}
---
+++ java_study/src/chapter01/Z_Basic.java
@@ -0,0 +1,35 @@
+package chapter01;
+
+public class Z_Basic {
+
+	public static void main(String[] args) {
+		// 자바 주석
+		// 단축키 : command + /
+		
+//		안녕하세요
+//		만나서
+//		반갑습니다.
+		
+		// 한 줄 주석 : 슬래시(/) 2개
+		// 여러 줄 주석
+		/*
+		 	슬래시 2개 사이에 별(*) 2
+		 */
+		
+		// 줄 복사 : command + option + 방향키 (ctrl + alt + 방향키)
+		// 줄 이동 : option + 방향키 (alt + 방향키)
+		// 줄 정렬 : command + shift + f
+		// 줄 삭제 : command + d
+		
+		// 파일 저장 : command + s
+		// 전체 파일 저장 : command + shift + s
+		
+		// 파일 실행 : command + f11
+		
+		// 이전 상황 돌아가기 : command + z
+		
+		
+
+	}
+
+}
